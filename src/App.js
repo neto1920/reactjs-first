a18fabbfd78cf81bd9bbe5ea97da7f24df334e15
neto1920 <neto.webprojetos@gmail.com>
2021-12-09 14:48:17 -0300

    Separando por pastas

--- src/App.js
+++ src/App.js
@@ -1,57 +1,29 @@
-import './App.css';
 import { Component } from 'react';
-import { PostCard } from './components/PostCard';
+
+import './App.css';
+import { Posts } from './components/Posts';
+
+import { loadPosts } from './utils/load-posts'
 
 class App extends Component {
-  // Criando objetos de estados, só precisei dos posts
   state = {
     posts: []
   }
 
-  componentDidMount() {
-    this.loadPosts();
+  async componentDidMount() {
+    await this.loadPosts();
   }
 
-  loadPosts = async () =>{
-    // chamando as api 
-    const postsResponse = fetch('https://jsonplaceholder.typicode.com/posts')
-    const photoReponse = fetch('https://jsonplaceholder.typicode.com/photos')
-    
-    // Transformando as api em arrays
-    const [posts, photos] = await Promise.all([postsResponse, photoReponse]);
-    
-    // Transformando as api em json
-    const postsJson = await posts.json();
-    const photosJson = await photos.json();
-
-    // Como as imagens tem mais quantidade que os postes, então fiz um map de "cada post tera uma imagem"
-    const postsAndPhotos = postsJson.map((post, index) => {
-      return { ...post, cover: photosJson[index].url}
-    });
-    
-    // setando os states
+  loadPosts = async () => {
+    const postsAndPhotos = await loadPosts();
     this.setState({ posts: postsAndPhotos });
   }
 
   render(){
-    // Chamando o estado post
     const { posts } = this.state;
-
-    // Renderizing my components
     return (
-      // Starting with a section for styling
-      <section className="container">
-        <div className="posts">              
-          {posts.map(post => ( // desestruturando meus post            
-            <PostCard 
-              key={post.id}
-              title={post.title}
-              body={post.body}
-              id={post.id}
-              cover={post.cover}
-            />
-          ))}
-        </div>
+      <section className="container">    
+        <Posts posts={posts} />    
       </section>
       
     );
